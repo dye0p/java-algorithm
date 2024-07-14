b9698eb41e8cd63fd2cb022edb8c9dbb96bb8d31
DongYeop <s01054830455@gmail.com>
2024-07-14 23:50:06 +0900

    대표값2

---
+++ basic/BOJ_2587.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_2587 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+        int[] arr = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int n = Integer.parseInt(st.nextToken());
+
+            sum += n;
+            arr[i] = n;
+        }
+
+        int res = sum / 5;
+        Arrays.sort(arr);
+
+        System.out.println(res);
+        System.out.println(arr[2]);
+    }
+}
