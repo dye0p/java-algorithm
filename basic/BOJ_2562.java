fed19d22d9b8155cff99a2036a20840413f7ff9a
DongYeop <s01054830455@gmail.com>
2024-07-16 19:30:57 +0900

    최댓값

---
+++ basic/BOJ_2562.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2562 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int res = 0;
+        int pos = 0;
+
+        StringTokenizer st;
+
+        for (int i = 0; i < 9; i++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+
+            if (n > res) {
+                res = n;
+                pos = i + 1;
+            }
+        }
+        System.out.println(res);
+        System.out.println(pos);
+    }
+}
