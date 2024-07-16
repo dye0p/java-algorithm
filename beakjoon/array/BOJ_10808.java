5d2afbdfe4596b410098a810a04b444a7b956689
DongYeop <s01054830455@gmail.com>
2024-07-16 19:59:50 +0900

    알파벳 개수

---
+++ beakjoon/array/BOJ_10808.java
@@ -0,0 +1,29 @@
+package array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_10808 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[26];
+
+        String s = br.readLine();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            arr[c - 97]++;
+        }
+        br.close();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
