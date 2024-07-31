8c70a8a54d74e10583a4e6160920e735a8c782a1
DongYeop <s01054830455@gmail.com>
2024-07-31 20:59:46 +0900

    신문 헤드라인

---
+++ algorithm/swea/lv1/SWEA_2047.java
@@ -0,0 +1,14 @@
+package swea.lv1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SWEA_2047 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        System.out.println(str.toUpperCase()); //소문자를 대문자로 변환
+    }
+}
