22c89f1063658f1cb62d4abf6e467766a2a57579
DongYeop <s01054830455@gmail.com>
2024-10-16 16:34:29 +0900

    [SWEA] 간단한 소인수분해/D2/retry

---
+++ algorithm/swea/lv2/SWEA_1945_RE.java
@@ -0,0 +1,36 @@
+package swea.lv2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SWEA_1945_RE {
+
+    static int[] res;
+    static int[] divide = {2, 3, 5, 7, 11};
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            res = new int[5];
+
+            int n = Integer.parseInt(br.readLine());
+
+            for (int i = 0; i < divide.length; i++) {
+                while (n % divide[i] == 0) {
+                    n /= divide[i];
+                    res[i] += 1;
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int result : res) {
+                sb.append(result).append(" ");
+            }
+            System.out.println("#" + test_case + " " + sb);
+        }
+    }
+}
