1ac7f08e94d58a070dac4c3c9990c70797608ad9
Lee Dong Yeop <s01054830455@gmail.com>
2025-06-08 01:41:36 +0900

    [Bronze V] Title: A-B, Time: 180 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.*;;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a - b);
+    }
+}
