41297d371a0b5fef9465fb6e63fdd815cd1191ca
Lee Dong Yeop <s01054830455@gmail.com>
2025-09-04 02:04:51 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 35.05 ms, Memory: 110 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public Stack<Integer> solution(int[] arr) {
+        
+        Stack<Integer> answer = new Stack<>();
+        answer.push(arr[0]);
+        
+        for(int i = 0; i < arr.length; i++) {
+            if(answer.peek() != arr[i]) {
+                answer.push(arr[i]);
+            }
+        }
+        return answer;
+    }
+}
