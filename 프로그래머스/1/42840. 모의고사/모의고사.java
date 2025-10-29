ffb778422a116e752a0fe6fb31b9ea2e5ea6d19a
Lee Dong Yeop <s01054830455@gmail.com>
2025-10-29 14:17:02 +0900

    [level 1] Title: 모의고사, Time: 1.43 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,46 +1,39 @@
 import java.util.*;
 
 class Solution {
-    public Set<Integer> solution(int[] answers) {
+    public List<Integer> solution(int[] answers) {
         
-        int[] one = {1,2,3,4,5};
-        int[] two = {2,1,2,3,2,4,2,5};
-        int[] three = {3,3,1,1,2,2,4,4,5,5};
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        int[] cnt = new int[3];
-        
+        int[] result = new int[3];
+       
         for(int i = 0; i < answers.length; i++) {
-            if(answers[i] == one[i % 5]) {
-                cnt[0]++;
+            if(answers[i] == a[i % a.length]) {
+                result[0]++;
             }
-            
-            if(answers[i] == two[i % 8]) {
-                cnt[1]++; 
+            if(answers[i] == b[i % b.length]) {
+                result[1]++;
             }
-            
-            if(answers[i] == three[i % 10]) {
-                cnt[2]++;
+            if(answers[i] == c[i % c.length]) {
+                result[2]++;
             }
         }
         
-        Set<Integer> arr = new HashSet<>();
+        int[] result2 = {result[0], result[1], result[2]};
         
-        int max = 0;
-        int index = 0;
+        Arrays.sort(result);
+        
+        List<Integer> answer = new ArrayList<>();
+        int max = result[2];
         for(int i = 0; i < 3; i++) {
-            if(max <= cnt[i]) {
-                 max = cnt[i];
-                 index = i;
+            if(result2[i] >= max) {
+                max = result2[i];
+                answer.add(i+1);
             }
         }
         
-        arr.add(index + 1);
-        
-        for(int i = 0; i < 3; i++ ) {
-                if(max == cnt[i]) {
-                    arr.add(i+1);
-                }
-            }
-        return arr;
+        return answer;
     }
 }
