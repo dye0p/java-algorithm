0feadae9c57b9f6bb058dab3df3fa76a76704ec5
Lee Dong Yeop <s01054830455@gmail.com>
2025-10-29 21:40:44 +0900

    [level 2] Title: 타겟 넘버, Time: 4.70 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,21 @@
+class Solution {
+    
+    int cnt = 0;
+    
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, target, 0, 0);
+        return cnt;
+    }
+    
+    private void dfs(int[] numbers, int target, int sum, int depth) {
+        if(depth == numbers.length) {
+            if(sum == target) { 
+                cnt++;
+            }
+            return; 
+        }
+        
+        dfs(numbers, target, sum + numbers[depth], depth+1);
+        dfs(numbers, target, sum - numbers[depth], depth+1);
+    }
+}
