d9e1c9d78780b4428a916a1b862a208472e00496
Lee Dong Yeop <s01054830455@gmail.com>
2025-11-05 22:34:49 +0900

    [level 1] Title: 과일로 만든 아이스크림 고르기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
+++ 프로그래머스/1/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
@@ -1,6 +1,7 @@
-select A.FLAVOR
-from FIRST_HALF A 
-join ICECREAM_INFO B 
-on A.FLAVOR = B.FLAVOR
-where (A.TOTAL_ORDER > 3000) and (B.INGREDIENT_TYPE	= 'fruit_based')
-order by A.TOTAL_ORDER desc
+select A.FLAVOR 
+from FIRST_HALF A
+join ICECREAM_INFO B on A.FLAVOR = B.FLAVOR
+where A.TOTAL_ORDER > 3000 AND B.INGREDIENT_TYPE = 'fruit_based'
+order by A.TOTAL_ORDER DESC
+
+
