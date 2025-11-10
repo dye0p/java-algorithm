68b5e28b93f7d79e17779f0dab488c2fe0072bf1
Lee Dong Yeop <s01054830455@gmail.com>
2025-11-10 16:54:15 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -0,0 +1,18 @@
+SELECT
+case 
+ when (PRICE < 10000) then 0
+ when (PRICE >= 10000 and PRICE < 20000) then 10000
+ when (PRICE >= 20000 and PRICE < 30000) then 20000
+ when (PRICE >= 30000 and PRICE < 40000) then 30000
+ when (PRICE >= 40000 and PRICE < 50000) then 40000
+ when (PRICE >= 50000 and PRICE < 60000) then 50000
+ when (PRICE >= 60000 and PRICE < 70000) then 60000
+ when (PRICE >= 70000 and PRICE < 80000) then 70000
+ when (PRICE >= 80000 and PRICE < 90000) then 80000
+ when (PRICE >= 90000 and PRICE < 100000) then 90000
+ end as PRICE_GROUP,
+count(PRICE) as PRODUCTS
+from PRODUCT
+group by PRICE_GROUP
+order by PRICE_GROUP
+
